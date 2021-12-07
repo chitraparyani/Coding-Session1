9e1a0b08c44c524509a3daab924223f4ff7cd2e0
chitraparyani <paryani.c@husky.neu.edu>
2021-12-06 19:26:25 -0500

    Adding selection sort

--- src/com/company/Sorting/SelectionSort.java
+++ src/com/company/Sorting/SelectionSort.java
@@ -1,4 +1,47 @@
 package com.company.Sorting;
 
+import java.util.Arrays;
+
+/*
+    Find minimum element from the pile and place it in correct place
+ */
 public class SelectionSort {
+
+    public void Selection_Sort(int[] arr) {
+
+        for(int i=0; i<arr.length; i++) {
+
+            int element1 = i;
+
+            for(int j=i+1; j<arr.length; j++) {
+
+                if(arr[j] < arr[element1]) {
+                    element1 = j;
+                }
+
+                if(i != element1)
+                    swap(arr, i, element1);
+            }
+        }
+
+    }
+
+    public void swap(int[] arr, int i, int j) {
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String args[]) {
+
+        int[] arr = {4, 5, 3, 2, 1};
+
+        SelectionSort selectionSort = new SelectionSort();
+        selectionSort.Selection_Sort(arr);
+
+        for(int a : arr) {
+            System.out.print(a + " ");
+        }
+    }
 }
